4c64f39732a2ead66e90e5ff6b89ecf9091ec1f0
Adriyan-Ahmed <adriyan.ahmed.jubayer@gmail.com>
2024-03-07 04:14:23 +0600

    Menu Page Created & Track Banner Added in the Menu Page

---
+++ src/Pages/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+const Menu = () => {
+    return (
+        <>
+            <section>
+                <div style={{ backgroundImage: `url('https://i.ibb.co/wYnDMD1/Footers-bg.png')` }} className="text-center py-10 md:py-16 lg:py-28 space-y-4 md:space-y-6 lg:space-y-10 bg-no-repeat bg-cover">
+                    <h1 className="text-3xl md:text-4xl lg:text-5xl  font-bold text-white">BLOG</h1>
+                    <h3 className="py-3.5 px-6 md:py-5 md:px-16 bg-gray-600 bg-opacity-50 text-base md:text-base inline-block rounded-[4px] font-semibold text-white"><span className="flex items-center gap-2"><IoHomeOutline></IoHomeOutline> HOME <IoIosArrowForward></IoIosArrowForward> MENU  </span></h3>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Menu;
