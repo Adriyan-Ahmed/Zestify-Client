a3e4dcb20bbee771448e51236b82ca119f9d02b3
Adriyan-Ahmed <adriyan.ahmed.jubayer@gmail.com>
2024-03-06 17:54:12 +0600

    Search Input Created

--- src/Components/Blog Card/BlogCard.jsx
+++ src/Components/Blog Card/BlogCard.jsx
@@ -1,4 +1,5 @@
 import { MdOutlineDateRange } from "react-icons/md";
+import { BiCategoryAlt } from "react-icons/bi";
 
 const BlogCard = ({blog}) => {
     
@@ -7,7 +8,11 @@ const BlogCard = ({blog}) => {
     const {title, banner_image, images, tags, category, posted_date, author, descriptions, goal_descriptions} = blog;
     return (
         <>
-            <div className=" flex flex-col justify-between gap-5 ">
+            <div data-aos="fade-up"
+                data-aos-offset="100"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-sine" 
+                className=" flex flex-col justify-between gap-5 ">
                 <div>
                     <img className="w-full rounded " src={banner_image} alt="" />
                 </div>
@@ -15,10 +20,13 @@ const BlogCard = ({blog}) => {
                     <div className="h-full flex flex-col gap-3 justify-between">
                         <h1 className="text-xl font-bold">{title}</h1>
                         <p className="text-sm md:text-base font-medium ">{descriptions.description1.slice(0, 80)} ...</p>
-                        <div className=" py-3 border-t border-b">
-                            <span className="flex items-center gap-2 text-sm md:text-base font-medium "><MdOutlineDateRange className="text-xl"></MdOutlineDateRange> {posted_date} </span>
+                        <div className="space-y-3">
+                            <div className=" py-3 border-t border-b flex justify-between">
+                                <span className="flex items-center gap-2 text-sm md:text-base font-medium "><MdOutlineDateRange className="text-xl"></MdOutlineDateRange> {posted_date} </span>
+                                <span className="flex items-center gap-2 text-sm md:text-base font-medium "><BiCategoryAlt className="text-xl"></BiCategoryAlt> {category} </span>
+                            </div>
+                            <button className=" text-[#F01543] w-full px-8 py-3.5 text-sm md:text-base lg:text-lg font-semibold border rounded btn-1 hover:text-white border-[#F01543] duration-1000 ">Read More</button>
                         </div>
-                        <button className=" text-[#F01543] w-full px-8 py-3.5 text-sm md:text-base lg:text-lg font-semibold border rounded btn-1 hover:text-white border-[#F01543] duration-1000 ">See More</button>
                     </div>
                 </div>
             </div>
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,6 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import BlogCard from "../Blog Card/BlogCard";
+import { IoIosSearch } from "react-icons/io";
+import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { BiCategoryAlt } from "react-icons/bi";
+
 
 const Blogs = () => {
 
@@ -22,13 +27,25 @@ const Blogs = () => {
         <>
             <div className="container mx-auto">
                 <div className="mx-4">
-                    <div className="lg:flex gap-6">
-                        <div className="lg:flex-[3] grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div className="flex flex-col-reverse xl:flex-row  gap-6">
+                        <div className="xl:flex-[5] grid grid-cols-1 md:grid-cols-2 gap-4">
                             {
                                 blogs.map((blog, i) => <BlogCard key={i} blog={blog} ></BlogCard>)
                             }
                         </div>
-                        <div className="lg:flex-[1]"></div>
+                        <div className="xl:flex-[2] space-y-8">
+                            <div className="p-5 bg-[#F9FAFB] rounded-md space-y-4 ">
+                                <h4 className="text-xl font-semibold">Search</h4>
+                                <div className="relative">
+                                    <input className="bg-white border p-5 w-full rounded-md outline-none " type="text" placeholder="What you Search?" required />
+                                    <span className="absolute top-2.5 right-2.5">
+                                        <button className=" text-[#F01543] p-3.5 text-sm md:text-base lg:text-lg font-semibold border rounded btn-1 hover:text-white border-[#F01543] duration-1000 "><IoIosSearch></IoIosSearch></button>
+                                    </span>
+                                </div>
+                            </div>
+
+
+                        </div>
                     </div>
                 </div>
             </div>
