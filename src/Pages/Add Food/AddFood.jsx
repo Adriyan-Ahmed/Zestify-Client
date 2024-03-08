7b74e0164033a40a7c6abf8134d758a68bf36664
Adriyan-Ahmed <adriyan.ahmed.jubayer@gmail.com>
2024-03-08 16:44:38 +0600

    General Information Providing Input Field Added

--- src/Pages/Add Food/AddFood.jsx
+++ src/Pages/Add Food/AddFood.jsx
@@ -25,7 +25,57 @@ const AddFood = () => {
                 <section>
                     <div className="container mx-auto">
                         <div className="mx-4">
-                            <div className="space-y-6"></div>
+                            <div className="space-y-6">
+
+
+                                <div className="flex items-center justify-between">
+                                    <h1 className="flex items-end gap-3"><img className="w-10" src="https://i.ibb.co/2y25x3W/add-food-icon.png" alt="" /> <span className="text-2xl font-semibold">Add New Food</span></h1>
+                                    <div className=" hidden md:flex flex-row-reverse items-center gap-5">
+                                        <button className="px-8 py-3.5 text-sm md:text-base lg:text-lg font-semibold border-2 rounded btn-1 hover:text-white hover:border-[#F01543] duration-1000 flex items-center gap-2 "><FaCheck></FaCheck> Add Food</button>
+                                        <span className="px-10 bg-[#F01543] rounded text-white font-bold py-3.5 text-sm md:text-base lg:text-lg hover:bg-transparent hover:py-2.5 border-[#F01543] hover:border-2 hover:text-[#F01543] focus:text-[#F01543] focus:border-2 focus:py-2.5 focus:bg-transparent duration-500 flex items-center gap-2 cursor-pointer "><IoSaveOutline></IoSaveOutline> Save Draft</span>
+                                    </div>
+                                </div>
+
+
+                                <div className="flex flex-col lg:flex-row gap-6">
+
+
+
+                                    <div className="flex-[7] space-y-9 ">
+
+
+                                        <div className="p-5 bg-[#F9FAFB] rounded-md space-y-4 ">
+                                            <h4 className="text-xl font-semibold">General Information</h4>
+
+                                            <div className="">
+                                                <span className="font-semibold ml-6">Food name</span>
+                                                <div className="p-2 bg-white rounded-md">
+                                                    <input className="bg-[#F9FAFB] p-5 w-full rounded-md outline-none " type="text" placeholder="Food Name" required />
+                                                </div>
+                                            </div>
+
+                                            <div className="">
+                                                <span className="font-semibold ml-6">Description Product</span>
+                                                <div className="p-2 bg-white rounded-md">
+                                                    <textarea className="bg-[#F9FAFB] p-5 w-full rounded-md outline-none " placeholder="Write a Description about your food ." name="description" id="" cols="" rows="6"></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+
+
+
+                                    <div className="flex-[3] space-y-5">
+                                        
+                                    </div>
+                                </div>
+
+
+                                <button className=" md:hidden mt-6 text-[#F01543] w-full px-8 py-3.5 text-sm md:text-base lg:text-lg font-semibold border rounded btn-1 hover:text-white border-[#F01543] duration-1000 flex items-center justify-center gap-3 "> <FaCheck></FaCheck> Add Food</button>
+
+                            </div>
                         </div>
                     </div>
                 </section>
